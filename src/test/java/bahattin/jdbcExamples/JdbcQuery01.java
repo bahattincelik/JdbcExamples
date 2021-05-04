c85ce3dd5bf36e860610b71d83236ffc102d4b67
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-05-04 15:22:50 +0300

    JDBC01

--- src/test/java/bahattin/jdbcExamples/JdbcQuery01.java
+++ src/test/java/bahattin/jdbcExamples/JdbcQuery01.java
@@ -1,10 +1,18 @@
 package bahattin.jdbcExamples;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcQuery01 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		//1) Ilgili driver'i yuklemeliyiz
+		Class.forName("oracle.jdbc.drive.OracleDriver");
 
+		//2) Baglanti olusturmaliyiz
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/ORCLCDB.localdomain",
+				"bahattin", "1234");
 	}
 
 }
